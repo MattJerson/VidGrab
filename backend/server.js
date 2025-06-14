1b5bf4624487e9a77d7e7006c5c7fa8b3ff283ea
Matt Jerson Figueroa <mattjersonn@gmail.com>
2025-06-14 18:57:34 +0800

    Port

--- backend/server.js
+++ backend/server.js
@@ -288,6 +288,6 @@ app.get("/api/proxy", async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`✅ Server running on port ${port}`);
 });
